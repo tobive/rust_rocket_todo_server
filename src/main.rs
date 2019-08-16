465497f0727afe65d3796e001cd7fb26ae85bd27
Katherine Philip <kurerunoshikaku@yahoo.com>
2019-08-16 12:05:42 +0700

    Change a bunch of stuffs to use iterators

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![feature(proc_macro_hygiene, decl_macro)]
+#![feature(proc_macro_hygiene, decl_macro, drain_filter)]
 
 #[macro_use]
 extern crate rocket;
@@ -7,17 +7,14 @@ use rocket::http::RawStr;
 use rocket::State;
 use rocket_contrib::json::Json;
 use serde::{Deserialize, Serialize};
-use std::fs::File;
-use std::fs::OpenOptions;
+use std::fs::{File, OpenOptions};
 use std::io::prelude::*;
-use std::io::BufReader;
-use std::io::Error;
-use std::io::SeekFrom;
+use std::io::{BufReader, Error, SeekFrom};
 use std::sync::Mutex;
 
 const TODO_FILE: &'static str = "foo.json";
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 struct Todo {
     title: String,
     content: String,
@@ -39,48 +36,44 @@ fn index() -> &'static str {
 
 #[get("/api/list?<query>")]
 fn return_list(query: Option<&RawStr>, new_state: State<NewAppState>) -> String {
-    let file_handle = new_state.todo_file.lock().unwrap().try_clone().unwrap();
-    let mut buf_reader = BufReader::new(file_handle);
+    let file_handle = new_state.todo_file.lock().unwrap();
+    let mut buf_reader = BufReader::new(&*file_handle);
+
     let data: TodoFile = read_data_to_json(&mut buf_reader).unwrap();
     match &query {
         None => {
             let string_json = serde_json::to_string(&data).unwrap();
-            println!("isi string_json: {}", string_json);
-            return format!("{}", &string_json);
+            format!("{}", &string_json)
         }
         Some(query) => {
-            let mut res: Option<String> = None;
-            for todo in &data.todo_list {
-                if query.to_string() == todo.title.to_string() {
-                    res = Some(todo.content.to_string());
-                }
-            }
-            match res {
+            let query = query.to_string();
+            match &data.todo_list.iter().filter(|todo| query == todo.title).nth(0) {
+                Some(todo) => format!("{:?}\n", todo),
                 None => format!("No todo with such title found.\n"),
-                Some(cont) => format!("{}\n", cont),
             }
         }
     }
 }
 
 #[post("/api/post", format = "application/json", data = "<todo>")]
-fn post_data<'r>(todo: Json<Todo>, new_state: State<'r, NewAppState>) -> String {
-    let (title, content) = (&todo.0.title, &todo.0.content);
+fn post_data(todo: Json<Todo>, new_state: State<NewAppState>) -> String {
+    let todo = todo.into_inner();
+    let Todo {title, content} = todo;
 
-    let mut file_handle = new_state.todo_file.lock().unwrap().try_clone().unwrap();
-    let mut data: TodoFile = read_data_to_json(&mut file_handle).unwrap();
+    let mut file_handle = new_state.todo_file.lock().unwrap();
+    let mut data = read_data_to_json(&mut *file_handle).unwrap();
     // Check if title already exist to prevent duplicate
-    for todo in &data.todo_list {
-        if title.to_string() == todo.title.to_string() {
-            return format!("title already exist. Can't save the item.\n");
-        }
-    }
 
-    let new_todo: Todo = Todo {
+    if let Some(_) = &data.todo_list.iter().find(|todo| title == todo.title) {
+        return format!("title already exist. Can't save the item.\n");
+    };
+
+    let new_todo = Todo {
         title: title.clone(),
         content: content.clone(),
     };
     data.todo_list.push(new_todo);
+
     let string_json = serde_json::to_string(&data).unwrap();
     println!("isi baru: {}", &string_json);
     let mut new_handler = File::create(TODO_FILE).unwrap();
@@ -92,23 +85,16 @@ fn post_data<'r>(todo: Json<Todo>, new_state: State<'r, NewAppState>) -> String 
 
 #[delete("/api/delete?<query>")]
 fn delete_item(query: String, new_state: State<NewAppState>) -> String {
-    let mut file_handle = new_state.todo_file.lock().unwrap().try_clone().unwrap();
-    let data: TodoFile = read_data_to_json(&mut file_handle).unwrap();
+    let mut file_handle = new_state.todo_file.lock().unwrap();
+    let mut data: TodoFile = read_data_to_json(&mut *file_handle).unwrap();
 
-    let mut temp_vec: Vec<Todo> = Vec::new();
-    for item in &data.todo_list {
-        if item.title.to_string() != query {
-            let todo = Todo {
-                title: item.title.to_string(),
-                content: item.content.to_string(),
-            };
-            temp_vec.push(todo);
-        }
-    }
+    let new_vec: Vec<Todo> = data.todo_list.drain_filter(|todo| todo.title == query).collect();
+
     let updated_list = TodoFile {
-        todo_list: temp_vec,
+        todo_list: new_vec,
     };
     let string_json = serde_json::to_string(&updated_list).unwrap();
+
     let mut new_handler = File::create(TODO_FILE).unwrap();
     match new_handler.write(string_json.as_bytes()).unwrap() {
         0 => format!("Error.\n"),
@@ -116,14 +102,14 @@ fn delete_item(query: String, new_state: State<NewAppState>) -> String {
     }
 }
 
-fn read_data_to_json<'a, T>(file: &'a mut T) -> Result<TodoFile, Error>
+fn read_data_to_json<T>(file: &mut T) -> Result<TodoFile, Error>
 where
     T: Seek + Read,
 {
     let mut contents = String::new();
-    file.seek(SeekFrom::Start(0)).unwrap(); // -> reset position to the start of file
-    file.read_to_string(&mut contents).unwrap();
-    let data: TodoFile = serde_json::from_str(&contents).unwrap();
+    file.seek(SeekFrom::Start(0))?; // -> reset position to the start of file
+    file.read_to_string(&mut contents)?;
+    let data= serde_json::from_str(&contents)?;
     Ok(data)
 }
 
